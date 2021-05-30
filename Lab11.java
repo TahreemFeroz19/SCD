da9a89abe3ddef59cbddf967eebe3837e3760ee9
TahreemFeroz19 <59493300+TahreemFeroz19@users.noreply.github.com>
2021-05-30 23:50:37 +0500

    Add files via upload

---
+++ Lab11.java
@@ -0,0 +1,27 @@
+package labs;
+
+public class Lab11 {
+int num;
+public boolean oddNum(int num) {
+	if(num%2==1)
+		return true;
+	return false;
+}
+public boolean evenNum(int num) {
+	if(num%2==0)
+		return true;
+	return false;
+}
+public boolean primeNum(int num) {
+	if(num%2==1)
+		return true;
+	return false;
+}
+public int factorial(int num) {
+	int fact=1;
+	for(int i=1;i<=num;i++)
+		fact=fact*i;
+	
+	return fact;
+}
+}
